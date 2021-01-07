632bf0aeee8983428474268b2d85128b964ab476
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2021-01-07 16:10:13 -0500

    added the player fall catcher

---
+++ Assets/Scripts/PlayerFallCatcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace KinematicCharacterController.Examples
+{
+    public class PlayerFallCatcher : MonoBehaviour
+    {
+
+        public GameObject Player;
+        public int respawnLayer = -200;
+
+        private void Update()
+        {
+            if(Player.transform.position.y <= respawnLayer)
+            {
+                Player.GetComponent<ExampleCharacterController>().MoveThePlayer(this.transform.position);
+
+            }
+        }
+
+
+
+
+
+    }
+}
