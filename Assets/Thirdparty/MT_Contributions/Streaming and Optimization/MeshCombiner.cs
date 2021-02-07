9ab54b71c537b39800f9ab40c26b137f33d356d5
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2021-02-07 13:28:21 -0500

    Experimenting with Mesh Instancing

--- Assets/Thirdparty/MT_Contributions/Streaming and Optimization/MeshCombiner.cs
+++ Assets/Thirdparty/MT_Contributions/Streaming and Optimization/MeshCombiner.cs
@@ -8,31 +8,40 @@ public class MeshCombiner : MonoBehaviour
 
     private void Start()
     {
-        //CombineMeshes();
+        CombineMeshes();
     }
 
     public void CombineMeshes()
     {
         MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
 
+        MeshRenderer renderer = GetComponentInChildren<MeshRenderer>();
+
         Debug.Log(name + " is combining" + filters.Length + "meshes!");
 
         Mesh finalMesh = new Mesh();
+        
 
         CombineInstance[] combiners = new CombineInstance[filters.Length];
 
-        for (int i = 0; i < filters.Length; i++)
+        Material finalMaterial = renderer.material;
+
+        for (int i = 1; i < filters.Length; i++)
         {
             combiners[i].subMeshIndex = 0;
             combiners[i].mesh = filters[i].sharedMesh;
             combiners[i].transform = filters[i].transform.localToWorldMatrix;
-            
+
+            Destroy(filters[i]);
 
         }
 
         finalMesh.CombineMeshes(combiners);
 
+        
+
         // always use SHARED MESH in editor
         GetComponent<MeshFilter>().sharedMesh = finalMesh;
+        //GetComponent<MeshRenderer>().material = finalMaterial;
     }
 }
