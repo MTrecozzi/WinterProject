fa72d91b59662910aae7f226adf6072288cb6c82
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2021-02-09 14:56:57 -0500

    added sustom method to Scene loding editor script

--- Assets/Editor/SceneLoader.cs
+++ Assets/Editor/SceneLoader.cs
@@ -39,6 +39,20 @@ public class SceneLoader : EditorWindow
         EditorSceneManager.OpenScene("Assets/Thirdparty/MT_Contributions/Grapple Snapple/Grapple UI.unity", OpenSceneMode.Additive);
     }
 
+    [MenuItem("SceneLoader/ LoadTomWorkStation")]
+    static void LoadTomWorkStation()
+    {
+        //LoadScenesFromSOArray("Assets/Scene Management/Scene Arrays/MT Work Station.asset");
+
+        //EditorSceneManager.OpenScene("Assets/Scenes/LevelDesign/LevelDesign11/LevelDesign11.unity", OpenSceneMode.Single);
+        EditorSceneManager.OpenScene("Assets/Thirdparty/MT_Contributions/Custom Character Controller/CharacterSetup V3.unity", OpenSceneMode.Additive);
+
+
+        EditorSceneManager.OpenScene("Assets/Scenes/TOM Ability Preferences.unity", OpenSceneMode.Additive);
+        EditorSceneManager.OpenScene("Assets/Thirdparty/MT_Contributions/Playtestr/GameStateManagementScene.unity", OpenSceneMode.Additive);
+        EditorSceneManager.OpenScene("Assets/Thirdparty/MT_Contributions/Grapple Snapple/Grapple UI.unity", OpenSceneMode.Additive);
+    }
+
     static void LoadScenesFromSOArray(string assetPath)
     {
 
