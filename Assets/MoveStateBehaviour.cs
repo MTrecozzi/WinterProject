32290968c25946f35cefa48a4eadc1491f0789df
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2021-03-22 18:26:08 -0400

    Created Empty MoveStateBehaviour

---
+++ Assets/MoveStateBehaviour.cs
@@ -0,0 +1,24 @@
+using KinematicCharacterController;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStateBehaviour : MonoBehaviour
+{
+
+    public void SetReferences(MovementState _managedState, MTCharacterController _controller, DefaultMoveStateBehaviour _defaultMoveState, KinematicCharacterMotor _motor)
+    {
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
